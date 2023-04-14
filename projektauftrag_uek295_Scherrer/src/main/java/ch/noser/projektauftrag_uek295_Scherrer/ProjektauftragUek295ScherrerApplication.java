96fb2c2f7ef6ebf5861f378465af131ad0dd597f
Samuel Scherrer <samuel.scherrer@edu.tbz.ch>
2023-04-14 12:10:53 +0200

    added new program 'Projektauftrag_uek295_Scherrer'

---
+++ projektauftrag_uek295_Scherrer/src/main/java/ch/noser/projektauftrag_uek295_Scherrer/ProjektauftragUek295ScherrerApplication.java
@@ -0,0 +1,13 @@
+package ch.noser.projektauftrag_uek295_Scherrer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektauftragUek295ScherrerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektauftragUek295ScherrerApplication.class, args);
+	}
+
+}
---
+++ projektauftrag_uek295_Scherrer/src/test/java/ch/noser/projektauftrag_uek295_Scherrer/ProjektauftragUek295ScherrerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noser.projektauftrag_uek295_Scherrer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektauftragUek295ScherrerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
