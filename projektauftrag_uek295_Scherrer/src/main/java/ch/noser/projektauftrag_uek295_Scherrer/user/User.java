6ecf03378ba1a10785b1a15df5e16ee4d90c8ac3
Samuel Scherrer <samuel.scherrer@edu.tbz.ch>
2023-04-17 21:43:24 +0200

    add Classes: 'User', 'UserRepository', 'UserService', 'Role', 'RoleRepository', 'RoleService'

---
+++ projektauftrag_uek295_Scherrer/src/main/java/ch/noser/projektauftrag_uek295_Scherrer/user/User.java
@@ -0,0 +1,32 @@
+package ch.noser.projektauftrag_uek295_Scherrer.user;
+
+import ch.noseryoung.exercise1.domain.role.Role;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "id_user", referencedColumnName = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "id_role", referencedColumnName = "role_id"))
+    private Set<Role> ruleSet;
+}
