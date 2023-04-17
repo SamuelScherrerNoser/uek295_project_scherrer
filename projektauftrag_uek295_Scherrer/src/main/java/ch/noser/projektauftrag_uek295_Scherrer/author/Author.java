c45ed407184d88cfa95cec6c0b1e51b2b73e5c94
Samuel Scherrer <samuel.scherrer@edu.tbz.ch>
2023-04-17 11:19:36 +0200

    add class 'Author'

---
+++ projektauftrag_uek295_Scherrer/src/main/java/ch/noser/projektauftrag_uek295_Scherrer/author/Author.java
@@ -0,0 +1,34 @@
+package ch.noser.projektauftrag_uek295_Scherrer.author;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Author")
+public class Author {
+
+    @Id
+    @NotBlank(message = "ID must be given.")
+    private Integer ID;
+
+    @NotBlank(message = "Name mustn't be empty.")
+    private String name;
+
+    @NotBlank(message = "Birthdate mustn't be empty.")
+    private String birthdate;
+
+    @Column(name = "profilePicture_URL")
+    private String profilePictureURL;
+}
